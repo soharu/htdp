962211f0478992337f2e14802e8d7ee6884c3e17
soharu <soharu1024@gmail.com>
2012-03-18 02:39:51 +0900

    solve exercise 3.1.2

--- chapter03/ex.3.1.1.rkt
+++ chapter03/ex.3.1.1.rkt
@@ -20,26 +20,33 @@
 ;; to compute the costs, given ticket-price
 (define (cost ticket-price)
   (+ 180.0
-     (* (attendees ticket-price) 0.04)))
+     (* .04 (attendees ticket-price))))
 
 ;; attendees : number  ->  number
 ;; to compute the number of attendees, given ticket-price
 (define (attendees ticket-price)
   (+ 120
-     (* (/ (- 5.0 ticket-price) 0.10)
-        15)))
+     (* (/ 15 .10) (- 5.0 ticket-price))))
 
 ;; Test
 (define delta 0.000000001)
+
+;; exercise 3.1.1
 (check-within (attendees 5.0) 120 delta)
 (check-within (attendees 4.0) 270 delta)
 (check-within (attendees 3.0) 420 delta)
 
+;; exercise 3.1.2
 (check-within (cost 5.0) 184.8 delta)
-(check-within (revenue 5.0) 600 delta)
-(check-within (profit 5.0) 415.2 delta)
+(check-within (cost 4.0) 190.8 delta)
+(check-within (cost 3.0) 196.8 delta)
+
+(check-within (revenue 5.0) 600.0 delta)
+(check-within (revenue 4.0) 1080.0 delta)
+(check-within (revenue 3.0) 1260.0 delta)
 
 (test)
+
 (profit 5.0) ; 415.2
 (profit 4.0) ; 889.2
 (profit 3.0) ; 1063.2
