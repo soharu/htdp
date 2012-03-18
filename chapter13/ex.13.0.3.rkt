6652de5479dc5a069b3b3199cb4a4e810ac78b7c
soharu <soharu1024@gmail.com>
2012-03-18 16:04:06 +0900

    solve exercise 13.0.3

---
+++ chapter13/ex.13.0.3.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(require lang/htdp-beginner)
+(require htdp/testing)
+
+;; 13.0.3-1
+(check-expect
+  (equal? (list 0 1 2 3 4 5)
+          (cons 0 (cons 1 (cons 2 (cons 3 (cons 4 (cons 5 empty)))))))
+  #t)
+
+;; 13.0.3-2
+(check-expect
+  (equal? (list (list 'adam 0) (list 'eve 1) (list 'louisXIV 2))
+          (cons (cons 'adam (cons 0 empty))
+                (cons (cons 'eve (cons 1 empty))
+                      (cons (cons 'louisXIV (cons 2 empty))
+                            empty))))
+  #t)
+
+;; 13.0.3-3
+(check-expect
+  (equal? (list 1 (list 1 2) (list 1 2 3))
+          (cons 1
+                (cons (cons 1 (cons 2 empty))
+                      (cons (cons 1 (cons 2 (cons 3 empty)))
+                            empty))))
+  #t)
+
+(test)
+
